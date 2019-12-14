07514a5c0d0e67fc7a3b8c066d7ce199f55c0edf
dixya123 <dixyamaharjan@sxc.edu.np>
2019-12-14 10:47:40 +0545

    newapp

--- qna_app/tests.py
+++ qna_app/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import CategoryModel
+
 
 # Create your tests here.
+class CategoryModelTest():
+    def setup(self):
+        self.django = CategoryModel.objects.Create(name='django' ,description='django unchained')
+    def test_foobar(self):
+        cat=CategoryModel.objects.get(name='django' ,description='django unchained')
+        self.assertEqual(cat,'django')
